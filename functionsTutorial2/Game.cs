a8d1745f5411cc77d2d532683d32ce84092f85cb
Hunter Trahan <s198054@students.aie.edu>
2019-08-21 08:29:20 -0500

    Add project files.

---
+++ functionsTutorial2/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionsTutorial2
+{
+    class Game
+    {
+
+        int playerHealth = 100;
+        int monsterDamage = 30;
+
+
+        public void Start()
+        {
+            Console.WriteLine("My game has begun!");
+            Console.ReadKey();
+
+
+            MonsterEncounter();
+            MonsterAttacks(monsterDamage * 2);
+            MonsterAttacks(monsterDamage);
+            MonsterAttacks(monsterDamage * 3);
+        }
+
+        private void MonsterEncounter()
+        {
+            Console.WriteLine("A vicious monster appears!");
+            Console.ReadKey();
+        }
+
+        private void MonsterAttacks(int damage)
+        {
+            playerHealth -= monsterDamage;
+            Console.WriteLine("The monster attacks you for " + damage + " points of damage. You have " + playerHealth + " health left.");
+            Console.ReadKey();
+        }
+    }
+}
